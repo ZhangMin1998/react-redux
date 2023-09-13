0b59883f2b62b31f80406b38633dfd387250e369
ZhangMin1998 <1464253423@qq.com>
2023-09-14 00:06:57 +0800

    组件修改store中的数据

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+import { add } from "./store/modules/counterStore"
 
 function App() {
   // 使用数据
   const { count } = useSelector(state => state.counter)
 
+  // 修改数据
+  const dispatch = useDispatch()
+
+
   const clickAdd = () => {
-    
+    // 1. 生成action对象
+    const action = add()
+    // 2. 提交action进行数据更新
+    dispatch(action)
   }
 
   return (
